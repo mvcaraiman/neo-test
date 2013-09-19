10b181b4dc757ce384287c0df10f508572973dbd
Vasile Caraiman <vca@bourse.lu>
2013-09-19 09:47:17 +0200

    set java compiler source to 5 ; add @Transactional on Repo.save

--- src/main/java/net/kama/Repo.java
+++ src/main/java/net/kama/Repo.java
@@ -3,12 +3,15 @@ package net.kama;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.neo4j.support.Neo4jTemplate;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public class Repo {
 	@Autowired
 	private Neo4jTemplate template;
 	
+	@Transactional(propagation=Propagation.REQUIRED)
 	public MyModel save(MyModel model){
 		return template.save(model);
 	}
